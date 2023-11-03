66fc4c9bfbc45502777cc6f84c35180d6207b030
LouMDVP11 <66640684+LouMDVP11@users.noreply.github.com>
2023-11-03 11:26:28 -0600

    Login Ready

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -13,9 +13,66 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: (() => Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: ((context) => const HomeScreen())))),
-        child: const Text('Iniciar sesión'));
+    var insertedEmail="";
+    var insertedPassword="";
+    var correctEmail="correo@test.com";
+    var correctPassword="ABC123";
+    TextEditingController _passwordController = TextEditingController();
+    TextEditingController _emailController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Parcial III'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            IconButton(onPressed: (){
+              showDialog(
+                context: context,
+                builder: ((context) {
+                  return SimpleDialog(
+                    title: const Text('Log In'),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                    children: [
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                        ),
+                        onChanged: (value) async {
+                          insertedEmail=value;
+                        },
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        onChanged: (value) async {
+                          insertedPassword=value;
+                        },
+                      ),
+                      FloatingActionButton(onPressed: (){
+                        
+                        if(insertedPassword==correctPassword && insertedEmail==correctEmail){
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: ((context) => const HomeScreen())));
+                        }
+                        setState(() {
+                          
+                        });
+                      },child:Icon(Icons.done),),
+                    ],
+                  );
+                }),
+              );
+              setState(() {
+                
+              });
+            }, icon: Icon(Icons.login)),
+          ]
+        ),
+      ),
+    );
   }
 }
